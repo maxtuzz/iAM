b8cf508ece77898a625f3d5619addd2c97a4c94d
Max Tuzzolino-Smith <devilmousedt@gmail.com>
2015-06-02 16:24:26 +1200

    More boilerplate added

---
+++ iam
@@ -0,0 +1,19 @@
+#! /usr/bin/python3
+# Iam Dev Codebase
+
+import sys
+import os
+
+#__author__ = "Max Tuzzolino-Smith"
+
+class iAM(object):
+    def main(self):
+        print(sys.argv)
+
+        if len(sys.argv) > 1:
+            print("Arguments found")
+        else:
+            print("iAM Help")
+
+iam = iAM()
+iam.main()
--- iam.py
+++ iam.py
@@ -8,12 +8,27 @@ import os
 
 class iAM(object):
     def main(self):
-        print(sys.argv)
-
         if len(sys.argv) > 1:
-            print("Arguments found")
+            if sys.argv == "-a" or sys.argv == "add":
+                self.add("testserver.auckland.ac.nz")
+            else:
+                self.connect(sys.argv[1])
         else:
             print("iAM Help")
+
+# Commands
+    def add(self, hostname):
+        print("Create a session here")
+
+    def remove(self):
+        print ("Remove a session here")
+
+    def search(self):
+        print("Searching for ")
+
+
+    def connect(self, host):
+        os.system("ssh " + host)
 
 iam = iAM()
 iam.main()
